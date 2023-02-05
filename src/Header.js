a6427720e3650c214efc02136e6eff7f80c1c55f
dighpamo <sputikov3150@gmail.com>
2023-02-05 22:51:27 +0300

    Application 1.1v

--- src/Header.js
+++ src/Header.js
@@ -1,21 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './css/Header.css';
 
 function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <header className="header">
       <Link to="/" className="header-logo">
         ToDo Application
       </Link>
-      <nav className="header-nav">
-        <Link to="/" className="header-nav-item">
-          Home
-        </Link>
-        <Link to="/about" className="header-nav-item">
-          About
-        </Link>
-      </nav>
+      <button onClick={() => setMenuOpen(!menuOpen)}>Open menu</button>
+      {menuOpen && (
+        <nav className="header-nav">
+          <Link to="/" className="header-nav-item">
+            Home
+          </Link>
+          <Link to="/about" className="header-nav-item">
+            About
+          </Link>
+        </nav>
+      )}
+      {menuOpen && <div className="overlay" />}
     </header>
   );
 }
